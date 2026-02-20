54d00c0b16f5c415eeba2922626e3c6827088e23
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2026-02-20 09:48:48 -0300

    note

---
+++ tests/WorkflowComplexTest.php
@@ -0,0 +1,175 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\Tests;
+
+use Chevere\Parameter\Attributes\_arrayp;
+use Chevere\Parameter\Attributes\_return;
+use Chevere\Parameter\Attributes\_string;
+use PHPUnit\Framework\TestCase;
+use function Chevere\Workflow\response;
+use function Chevere\Workflow\run;
+use function Chevere\Workflow\sync;
+use function Chevere\Workflow\workflow;
+
+final class WorkflowComplexTest extends TestCase
+{
+    public function testAppNew(): void
+    {
+        $workflow = workflow(
+            appAssertDomainAvailable: sync(
+                fn (): null => null,
+            ),
+            user: sync(
+                #[_return(
+                    new _arrayp(
+                        name: new _string(),
+                        email: new _string()
+                    )
+                )]
+                fn (): array => [
+                    'name' => 'User Name',
+                    'email' => 'user@example.com',
+                ],
+            ),
+            plan: sync(
+                #[_return(
+                    new _arrayp(
+                        currency_code: new _string()
+                    )
+                )]
+                fn (): array => [
+                    'currency_code' => 'usd',
+                ],
+            ),
+            subCreate: sync(
+                fn (string $currency_code): int => 1,
+                currency_code: response('plan', 'currency_code')
+            ),
+            refreshUserSharedCache: sync(
+                fn (): null => null,
+            ),
+            appCreate: sync(
+                fn (int $sub_id): int => 1,
+                sub_id: response('subCreate'),
+            ),
+            orderCreate: sync(
+                fn (int $product_id): int => 1,
+                product_id: response('appCreate'),
+            ),
+            subOrderCreate: sync(
+                fn (int $sub_id, int $order_id): int => 1,
+                order_id: response('orderCreate'),
+                sub_id: response('subCreate'),
+            ),
+            checkoutCreate: sync(
+                fn (int $order_id, int $product_id): int => 1,
+                order_id: response('orderCreate'),
+                product_id: response('appCreate')
+            ),
+            appIdCloak: sync(
+                fn (int $id): string => 'cloak' . $id,
+                id: response('appCreate'),
+            ),
+            appUri: sync(
+                fn (string ...$value): string => '',
+                idCloak: response('appIdCloak'),
+            ),
+            serverAppCreateArgs: sync(
+                function (
+                    int $id,
+                ): array {
+                    return get_defined_vars();
+                },
+                id: response('appCreate')
+            ),
+            jobPushServerAppCreate: sync(
+                fn (
+                    mixed $arguments = [],
+                ): null => null,
+                arguments: response('serverAppCreateArgs'),
+            ),
+            emailAppContext: sync(
+                fn (string $uri, string $idCloak): array => [],
+                uri: response('appUri'),
+                idCloak: response('appIdCloak'),
+            ),
+            emailAppArgs: sync(
+                function (
+                    string $toEmail,
+                    string $toName,
+                    array $context
+                ): array {
+                    return get_defined_vars();
+                },
+                toEmail: response('user', 'email'),
+                toName: response('user', 'name'),
+                context: response('emailAppContext'),
+            ),
+            jobPushEmailApp: sync(
+                fn (
+                    mixed $arguments = [],
+                ): null => null,
+                arguments: response('emailAppArgs'),
+            ),
+        );
+        // Workflow doesn't respect this sort because it uses levels for building the dependency graph.
+        // Will push non-dependent jobs to be executed first, regardless of their declaration order.
+        // Use `->withDepends('jobName')` to force the order of execution.
+        // $expected = [
+        //     ['appAssertDomainAvailable'],
+        //     ['user'],
+        //     ['plan'],
+        //     ['subCreate'],
+        //     ['refreshUserSharedCache'],
+        //     ['appCreate'],
+        //     ['orderCreate'],
+        //     ['subOrderCreate'],
+        //     ['checkoutCreate'],
+        //     ['appIdCloak'],
+        //     ['appUri'],
+        //     ['serverAppCreateArgs'],
+        //     ['jobPushServerAppCreate'],
+        //     ['emailAppContext'],
+        //     ['emailAppArgs'],
+        //     ['jobPushEmailApp'],
+        // ];
+        // $this->assertSame(
+        //     $expected,
+        //     $workflow->jobs()->graph()->toArray()
+        // );
+        $this->assertSame(
+            [
+                ['appAssertDomainAvailable'],
+                ['user'],
+                ['plan'],
+                ['refreshUserSharedCache'],
+                ['subCreate'],
+                ['appCreate'],
+                ['orderCreate'],
+                ['subOrderCreate'],
+                ['appIdCloak'],
+                ['checkoutCreate'],
+                ['appUri'],
+                ['serverAppCreateArgs'],
+                ['jobPushServerAppCreate'],
+                ['emailAppContext'],
+                ['emailAppArgs'],
+                ['jobPushEmailApp'],
+            ],
+            $workflow->jobs()->graph()->toArray()
+        );
+        $run = run($workflow);
+        $this->assertCount(0, $run->skip());
+    }
+}
